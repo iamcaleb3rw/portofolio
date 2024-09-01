88091da218997e57150bfaa68fbf3f88545f065e
Caleb Ganza <icaleb130@gmail.com>
2024-09-01 22:21:05 +0200

    Adding the first template

--- app/templates/page.tsx
+++ app/templates/page.tsx
@@ -1,13 +1,30 @@
 import Meteors from "@/components/magicui/meteors";
+import { Button } from "@/components/ui/button";
 import React from "react";
+import Link from "next/link";
+import templateImg from "@/public/templ.png";
+import Image from "next/image";
 
 const Tools = () => {
   return (
-    <div className="relative flex h-[300px] w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl">
-      <Meteors number={60} />
-      <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-4xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">
-        Coming Soon
-      </span>
+    <div className="border p-3 mt-4 flex flex-col lg:flex-row gap-4 rounded-2xl hover:bg-secondary/50 transition-[all_10s]">
+      <Image
+        src={templateImg}
+        alt="banner"
+        className="w-full md:w-80 border rounded-xl"
+      />
+      <div className="flex flex-col gap-2">
+        <h1 className="text-xl font-semibold">AI SaaS template</h1>
+        <p className="text-muted-foreground">
+          Tech themed websited. Suitable for SaaS startups. Contact me to
+          acquire the template
+        </p>
+        <Link href="https://aitemplate.vercel.app" target="_blank">
+          <Button className="w-full dark:bg-purple-600 dark: text-white">
+            Live preview
+          </Button>
+        </Link>
+      </div>
     </div>
   );
 };
